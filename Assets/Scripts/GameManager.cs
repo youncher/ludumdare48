39c395136091347fb2bfc62700396da5a11ced55
youncher <44423254+youncher@users.noreply.github.com>
2021-04-26 17:13:53 -0700

    Add target counting - WIP

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,21 +1,41 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public int killsNeededToWin { get; set; }
+    
     public GameObject playerGO;
     public GameObject gameOverDisplayGO;
     public GameObject gloomProjectileControllerGO;
+    public GameObject targetCounterTextGO;
     
+    private const int MAX_LEVEL_TO_WIN = 3;
     private PlayerController player;
     private GameOverDisplay gameOverDisplay;
     private GloomProjectileController gloomProjectileController;
+    private TextMeshProUGUI targetCounterText;
     
-    private void Start()
+    private int currentLevel = 1;
+
+
+    void Start()
     {
         gameOverDisplay = gameOverDisplayGO.GetComponent<GameOverDisplay>();
         player = playerGO.GetComponent<PlayerController>();
         gloomProjectileController = gloomProjectileControllerGO.GetComponent<GloomProjectileController>();
+        targetCounterText = targetCounterTextGO.GetComponent<TextMeshProUGUI>();
+
+        if (string.Equals(SceneManager.GetActiveScene().name, "Level1Scene"))
+        {
+            Debug.Log("I'M IN SCENE 1!!!");
+        }
+
+        // Display number of kills to win level
+        killsNeededToWin = 3;
+        targetCounterText.SetText($"{killsNeededToWin}");
     }
     
     // Parameter: string - "win" or "lose"
@@ -35,5 +55,33 @@ public class GameManager : MonoBehaviour
     public void LoadMenu()
     {
         SceneManager.LoadScene("IntroScene");
+    }
+
+    // Advance user to next level
+    public void AdvanceNextLevel()
+    {
+        if (currentLevel >= MAX_LEVEL_TO_WIN)
+        {
+            Debug.Log("current level:" + currentLevel);
+            Debug.Log("MAX_LEVEL_TO_WIN:" + MAX_LEVEL_TO_WIN);
+            Debug.Log("You win!");
+            HandleGameOver("win");
+        }
+        //
+        // int nextLevel = currentLevel + 1;
+        //
+        // if (nextLevel == 2)
+        // {
+        //     Debug.Log("Advancing to Level 2");
+        //     killsNeededToWin = 4;
+        // } else if (nextLevel == 3)
+        // {
+        //     Debug.Log("Advancing to Level 3");
+        //     killsNeededToWin = 5;
+        // }
+        //
+        // currentLevel = nextLevel;
+        
+        // TODO advance to next level
     }
 }
--- Assets/Scripts/KillIndicator.cs
+++ Assets/Scripts/KillIndicator.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 public class KillIndicator : MonoBehaviour
 {
     public TextMeshProUGUI text;
-    
+
+    public GameManager gameManager;
     public int KillCount = 0;
     public int ToWin = 3;
+    
     private void Start()
     {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         text = GameObject.Find("KillCount").GetComponent<TextMeshProUGUI>();
         text.SetText(KillCount.ToString());
     }
 
+    void Update()
+    {
+        if (KillCount >= ToWin)
+        {
+            gameManager.AdvanceNextLevel();
+        }
+    }
+    
     // add one kill and updates UI
     public void IncrementKills()
     {
